b74cbcd09e217685282d7f8bb014a308ee0d82d5
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2023-09-16 10:17:14 +0800

    Initial commit

---
+++ src/main/java/nat/weatherwisebackend/WeatherWiseBackendApplication.java
@@ -0,0 +1,13 @@
+package nat.weatherwisebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherWiseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherWiseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nat/weatherwisebackend/WeatherWiseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nat.weatherwisebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherWiseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
